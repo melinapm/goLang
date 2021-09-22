da1026d434f16b4c7252d1f5848290b03b8b1d89
Melina Perez Mounet <meli.pm@gmail.com>
2021-09-22 09:19:31 -0300

    Se acomoda el campo Length

--- tp/TP.go
+++ tp/TP.go
@@ -2,37 +2,39 @@ package tp
 
 import (
 	"errors"
+	"fmt"
 	"strconv"
 )
 
 type Result struct {
 	Type   string
-	Lenght int
+	Length int
 	Value  string
 }
 
-func GenerateStruct(str string) (*Result, error) {
+func GenerateStruct(str string) (Result, error) {
+	fmt.Println("llego?")
 	r := Result{
 		Type:   "",
 		Value:  "",
-		Lenght: 0,
+		Length: 0,
 	}
 
 	s := []rune(str)
 
 	r.Type = string(s[0]) + string(s[1])
 	if r.Type != "TX" && r.Type != "NN" {
-		return &r, errors.New("Is not a valid type")
+		return r, errors.New("Is not a valid type")
 	}
 
 	if int(s[3]-'0') == 0 {
-		r.Lenght = int(s[2]-'0') + 9
+		r.Length = int(s[2]-'0') + 9
 	} else {
-		r.Lenght = int(s[2]-'0') + int(s[3]-'0')
+		r.Length = int(s[2]-'0') + int(s[3]-'0')
 	}
 
-	if r.Lenght != len(s)-4 {
-		return &r, errors.New("The lenght is invalid")
+	if r.Length != len(s)-4 {
+		return r, errors.New("The Length is invalid")
 	}
 
 	for i := 4; i < len(s); i++ {
@@ -41,13 +43,13 @@ func GenerateStruct(str string) (*Result, error) {
 
 	if r.Type == "TX" {
 		if _, err := strconv.Atoi(r.Value); err == nil {
-			return &r, errors.New("Is not a text")
+			return r, errors.New("Is not a text")
 		}
 	} else if r.Type == "NN" {
 		if _, err := strconv.Atoi(r.Value); err != nil {
-			return &r, errors.New("Is not a number")
+			return r, errors.New("Is not a number")
 		}
 	}
 
-	return &r, nil
+	return r, nil
 }
